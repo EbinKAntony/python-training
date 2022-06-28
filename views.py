adff7798d41dba4312fe1a47a214e3c60d24b38c
bipinchacko <54311317+bipinchacko@users.noreply.github.com>
2022-06-28 09:35:57 +0530

    Update views.py

--- views.py
+++ views.py
@@ -27,3 +27,37 @@ def index(request):
                     script_obj.append(mandpolicy_obj)
     aaaa = MandatoryAppPolicy.objects.bulk_create(script_obj)
     return render(request, "index.html")
+
+
+
+def index(request):
+    script_obj = []
+    dict1 = {
+        1:{"preinstall_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"},
+        "postinstall_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"},
+        "audit_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"}},
+        2:{"preinstall_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"},
+        "postinstall_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"},
+        "audit_script":{"script_file_source":"upload_file","file_name":"aa.pdf","binary_path":"aaaaaa/aa","arguments":"hiiiii"}}   
+        }
+    SCRIPT_FILE_S ={"upload_file":1,"hexnosde_repository":2}
+    # SCRIPT_TYPE = {"pre_install":1,"post_install":2,"audit":3}
+    # print(dict1)
+    for i,v in dict1.items():
+        if i==1:
+            # for k,j in v.items():
+            #     if k == "postinstall_script":
+                    # MandatoryAppPolicy.objects.filter(script_file_source = 1,script_type = 3).delete()
+            data = v.get("postinstall_script",None) or v.get("preinstall_script",None) or {}
+            if data:=v.get("postinstall_script",None):
+                
+
+            data['type'] = "postinstall_script" if v.get("postinstall_script",None) else "preinstall_script"
+            # x = {"script_type":3}
+            # j.update(x)
+            # j['script_file_source'] = SCRIPT_FILE_S.get(j.get('script_file_source'))
+            # mandpolicy_obj = MandatoryAppPolicy(**j)
+            # script_obj.append(mandpolicy_obj)
+    # aaaa = MandatoryAppPolicy.objects.bulk_create(script_obj)
+    return render(request, "index.html")
+
